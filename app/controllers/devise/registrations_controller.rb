123abce508488202c08150a3b55c21f36fc7b144
Patrick Keller <byteclic@gmail.com>
2012-09-10 22:26:27 +0300

    Update app/controllers/devise/registrations_controller.rb

--- app/controllers/devise/registrations_controller.rb
+++ app/controllers/devise/registrations_controller.rb
@@ -1,6 +1,8 @@
 class Devise::RegistrationsController < DeviseController
   prepend_before_filter :require_no_authentication, :only => [ :new, :create, :cancel ]
   prepend_before_filter :authenticate_scope!, :only => [:edit, :update, :destroy]
+  
+  load_and_authorize_resource
 
   # GET /resource/sign_up
   def new
