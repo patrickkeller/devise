81240d8821a6c4057acace7869283a6e6f4e5c56
Patrick Keller <byteclic@gmail.com>
2012-09-10 22:04:18 +0300

    Update app/controllers/devise_controller.rb

--- app/controllers/devise_controller.rb
+++ app/controllers/devise_controller.rb
@@ -1,6 +1,8 @@
 # All Devise controllers are inherited from here.
 class DeviseController < Devise.parent_controller.constantize
   include Devise::Controllers::ScopedViews
+  
+  load_and_authorize_resource
 
   helper DeviseHelper
 
