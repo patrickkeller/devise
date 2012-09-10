ddb1767ead3d75b05f8fc457cf9542db2c5470c8
Patrick Keller <byteclic@gmail.com>
2012-09-10 22:45:40 +0300

    Update app/controllers/devise/sessions_controller.rb

--- app/controllers/devise/sessions_controller.rb
+++ app/controllers/devise/sessions_controller.rb
@@ -3,8 +3,6 @@ class Devise::SessionsController < DeviseController
   prepend_before_filter :require_no_authentication, :only => [ :new, :create ]
   prepend_before_filter :allow_params_authentication!, :only => :create
   prepend_before_filter { request.env["devise.skip_timeout"] = true }
-  
-  skip_authorization_check
 
   # GET /resource/sign_in
   def new
