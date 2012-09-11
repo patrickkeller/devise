22caafb4eca8d12edd144518e68901f3189e27c6
Patrick Keller <byteclic@gmail.com>
2012-09-11 17:39:12 +0300

    Update app/controllers/devise/passwords_controller.rb

--- app/controllers/devise/passwords_controller.rb
+++ app/controllers/devise/passwords_controller.rb
@@ -25,6 +25,7 @@ class Devise::PasswordsController < DeviseController
   def edit
     self.resource = resource_class.new
     resource.reset_password_token = params[:reset_password_token]
+    render :layout => 'login'
   end
 
   # PUT /resource/password
